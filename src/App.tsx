3410b80c8d931bfa0862d6312e7db575a53f1bf2
Sanket Naik <sanketnaik99@gmail.com>
2023-02-16 20:29:15 -0800

    Installs TypeScript & updates root level components
    - Installed typescript and types for all dependencies.
    - Converted root level components to TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import YDXHome from "./pages/YDXHome";
+import PageNotFound from "./pages/PageNotFound";
+import UserStudyHome from "./pages/UserStudyHome";
+import PlayVideo from "./pages/PlayVideo";
+import "./assets/css/index.css";
+import { ToastContainer, toast, Zoom } from "react-toastify"; // for toast messages
+import "react-toastify/dist/ReactToastify.css";
+import LogRocket from "logrocket";
+
+const App = () => {
+  useEffect(() => {
+    if (process.env.REACT_APP_ENVIRONMENT === "production") {
+      LogRocket.init(process.env.REACT_APP_LOGROCKET_ID || "", {
+        mergeIframes: true,
+        childDomains: ["*"],
+      });
+    }
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/:youtubeVideoId/:userId" element={<YDXHome />} />
+        <Route path="/*" element={<PageNotFound />} />
+        <Route path="/userstudy/:participantId" element={<UserStudyHome />} />
+        <Route path="/videopage/:youtubeVideoId" element={<PlayVideo />} />
+      </Routes>
+      <ToastContainer
+        className="toast-btn"
+        position="top-center"
+        autoClose={1000}
+        closeOnClick
+        draggable
+        pauseOnFocusLoss
+        pauseOnHover
+        theme="colored"
+      />
+    </BrowserRouter>
+  );
+};
+
+export default App;
