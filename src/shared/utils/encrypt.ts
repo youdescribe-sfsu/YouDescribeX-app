a959d812c2ec5418aa0b6233978fc32f825e2765
SFState <vishalsharma1907@gmail.com>
2023-07-28 10:00:45 -0700

    Added crpyto code

--- src/shared/utils/encrypt.ts
+++ src/shared/utils/encrypt.ts
@@ -1,8 +1,9 @@
 import CryptoJS from 'crypto-js'
 
 const encryptData = (userDataString: string) => {
+  console.log('userDataString', userDataString)
   const encryptedData: string = CryptoJS.AES.encrypt(
-    'userDataString',
+    userDataString,
     '050cf42ee14d597188b0695a94df5e866d7eda5d06af32ff3ac329ddbcf7ca8a',
   ).toString()
   console.log('encryptedData::', encryptedData)
