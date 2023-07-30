824d4ef9bdd2a6d2b2043858a5a5b72bb97db4f4
SFState <vishalsharma1907@gmail.com>
2023-07-30 01:11:00 -0700

    fixed useeffect

--- src/pages/Wishlist/Wishlist.tsx
+++ src/pages/Wishlist/Wishlist.tsx
@@ -166,7 +166,7 @@ const Wishlist = () => {
     document.title = translate('YouDescribe - Wish List')
     loadTableVideos(currentPageNumber, perPage)
     loadTopVideos()
-  }, [userDataStore])
+  }, [userDataStore.getState().userId])
 
   /*
     Loads data for the table using the /wishlist/search endpoint
