06e2c11e7ecb9bca6ea8f89a58d397d5a219ed88
SFState <vishalsharma1907@gmail.com>
2023-09-20 14:59:21 -0700

    Added audiodescription header

--- src/pages/YDXHome.tsx
+++ src/pages/YDXHome.tsx
@@ -352,7 +352,7 @@ const YDXHome = () => {
           }`,
           {
             headers: {
-              audio_description_id: audioDescriptionId,
+              audiodescription: audioDescriptionId,
             },
           },
         )
