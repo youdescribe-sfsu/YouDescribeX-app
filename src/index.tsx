0010609cb07f02ea93e964906d3ad8431a11e5e3
SFState <vishalsharma1907@gmail.com>
2023-11-20 10:56:22 -0800

    Add "@alwaysmeticulous/recorder-loader" as a dependency and implement Meticulous recorder for non-production environments

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,50 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+
+
+
+
+import { tryLoadAndStartRecorder } from '@alwaysmeticulous/recorder-loader'
+
+async function startApp() {
+    // Record all sessions on localhost, staging stacks and preview URLs
+    if (!isProduction()) {
+      // Start the Meticulous recorder before you initialise your app.
+      // Note: all errors are caught and logged, so no need to surround with try/catch
+      await tryLoadAndStartRecorder({
+        projectId: 'VJhstm4BWoFqSY6iayLo6WHvKEohMlajw92jcuda',
+        isProduction: false,
+      });
+    }
+
+    // Initalise app after the Meticulous recorder is ready, e.g.
+    ReactDOM.render(<React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>, document.getElementById('root'));
+}
+
+function isProduction() {
+    // TODO: Update me with your production hostname
+    return window.location.hostname.indexOf("your-production-site.com") > -1;
+}
+
+startApp();
+
+
+
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
