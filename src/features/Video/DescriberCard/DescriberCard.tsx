04e2c2bb1f197eb192f37e059c251c5c88fd310a
Alekhya1311 <alekhyabairaboina1311@gmail.com>
2024-03-08 12:04:30 -0800

    if the desc is AI, removing profile picture

--- src/features/Video/DescriberCard/DescriberCard.tsx
+++ src/features/Video/DescriberCard/DescriberCard.tsx
@@ -102,7 +102,9 @@ const DescriberCard = ({
       <div className="w3-card-2">
         <div className="w3-row">
           <div className="w3-col l3 m5 s3">
-            <img src={picture} alt={`Profile picture of ${name}`} />
+            {name !== 'AI Description Draft' && (
+              <img src={picture} alt={`Profile picture of ${name}`} />
+            )}
           </div>
           <div className="w3-col l9 m7 s9">
             {name}
